e2a1324f183d3391e80dbf856ee5681809ee7478
Anjas amar pradana <anjastech01@gmail.com>
2018-09-07 12:34:57 +0700

    Add files via upload

---
+++ themes/wowonder2/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'WoWonder2';
+
+$themeFolder = 'wowonder2';
+
+$themeAuthor = 'Anjas Amar Pradana';
+
+$themeAuthorUrl = 'mailto:anjastech01@gmail.com';
+
+$themeVirsion = '1.5.2';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
