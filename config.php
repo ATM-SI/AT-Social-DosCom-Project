8668d672ebe0d0305efef9d54de8004993761264
Anjas amar pradana <anjastech01@gmail.com>
2018-09-07 10:32:13 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Anjas Amar Pradana
+// |
+// | 
+// | @author_email: anjastech01@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2018 ANJAS-TECH-MEDIA. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User 
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "at-social";
+
+// Site URL
+$site_url = "http://localhost/at-social"; // e.g (http://example.com)
+
+
+$purchase_code = "123456"; 
+?>
